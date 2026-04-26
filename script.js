fd9558f89d6c66102773192d9ad00375c22951a9
Sunil Gautam <gautamsunil917@gmail.com>
2026-04-26 23:12:32 +0545

    javascript started with real data

--- script.js
+++ script.js
@@ -1,2 +1,64 @@
 console.log("we are running")
 
+const data = {
+    brightness : {
+        max : 100,
+        min : 0,
+        value : 30,
+        unit : "%",
+    },
+    contrast  : {
+          max : 100,
+        min : 0,
+        value : 30,
+        unit : "%",
+    },
+    exposure  : {
+          max : 100,
+        min : 0,
+        value : 30,
+        unit : "%",
+    },
+    saturation  : {
+          max : 100,
+        min : 0,
+        value : 30,
+        unit : "%",
+    },
+    hueRotation  : {
+          max : 100,
+        min : 0,
+        value : 30,
+        unit : "deg",
+    },
+    blur  : {
+          max : 100,
+        min : 0,
+        value : 30,
+        unit : "px",
+    }, 
+    grayscale  : {
+          max : 100,
+        min : 0,
+        value : 30,
+        unit : "px",
+    }, 
+    sepia  : {
+          max : 100,
+        min : 0,
+        value : 30,
+        unit : "p%",
+    }, 
+    opacity  : {
+          max : 100,
+        min : 0,
+        value : 30,
+        unit : "p%",
+    }, 
+    invert  : {
+          max : 100,
+        min : 0,
+        value : 30,
+        unit : "p%",
+    }
+}
